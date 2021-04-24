c326f8fffb5caffb22d994d56dd36effdb4e4c9b
Oswaldo Rosales <168518@ust-global.com>
2021-04-24 12:53:10 -0500

    chore: add code base with springboot

---
+++ src/test/java/org/gob/mex/covid/CovidStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gob.mex.covid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
